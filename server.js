456ca70d25cfc6cb24cdeef3d798f3df69175483
senthilkmu01 <senthilkmu01@gmail.com>
2016-09-18 11:57:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articleone',function(req,res)
+{
+    res.send('This is article one');
+});
+
+app.get('/articleSecond',function(req,res)
+{
+    res.send('This is article Second');
+});
+
+app.get('/articlethree',function(req,res)
+{
+    res.send('This is article three');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
