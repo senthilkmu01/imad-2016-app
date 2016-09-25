2d9dcb3b02bd12707db1c8536374405eee4c32c3
senthilkmu01 <senthilkmu01@gmail.com>
2016-09-25 20:01:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var button=document.getElementbyId('btncounter');
+var counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementbyId('count');
+    span.innerHTML=counter.toString();
+};
