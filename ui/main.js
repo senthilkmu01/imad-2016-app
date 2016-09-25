c11cabd40789eb25045f5452677bd9a0b4e8b3ee
senthilkmu01 <senthilkmu01@gmail.com>
2016-09-25 21:00:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,15 @@
-console.log('Loaded!');
+
 var button=document.getElementbyId('btncounter');
 
 button.onclick=function(){
+    
     var request=new XMLHttpRequest();
     
     request.onreadystatechange=function()
     {
       if(request.readyState==XMLHttpRequest.DONE)
       {
-          if(request.status==200)
+          if(request.status === 200)
           {
               var counter=request.responseText;
               counter=counter+1;
